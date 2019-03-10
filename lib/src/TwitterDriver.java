1ee6ec81bf0ab40abee6cc5fcd1b75fdd717ce45
binary-knight <jason.s.knight@gmail.com>
2019-03-10 15:40:53 -0500

    Added basic text UI
    Added basic text UI for user to input lat/long/miles/date-range/search query.  Needs a lot of work still.

--- lib/src/TwitterDriver.java
+++ lib/src/TwitterDriver.java
@@ -20,19 +20,49 @@ public class TwitterDriver {
 
         Twitterer bigBird = new Twitterer(consolePrint);
 
-        String message = "I'm testing out the twitter4j API for Java.";
-        bigBird.tweetOut(message);
+//        String message = "I'm testing out the twitter4j API for Java.";
+//        bigBird.tweetOut(message);
+
+//        Scanner scan = new Scanner(System.in);
+//        System.out.println("Please enter a Twitter handle, do not include the '@' symbol (or 'done' to quit.)");
+//        String twitter_handle = scan.next();
+//        while (!"done".equals(twitter_handle))
+//        {
+//            bigBird.queryHandle(twitter_handle);
+//            System.out.println("Please enter a Twitter handle, do no include the '@' symbol (or 'done' to quit.)");
+//            twitter_handle = scan.next();
+//        }
 
         Scanner scan = new Scanner(System.in);
-        System.out.println("Please enter a Twitter handle, do not include the '@' symbol (or 'done' to quit.)");
-        String twitter_handle = scan.next();
-        while (!"done".equals(twitter_handle)) ;
-        {
-            bigBird.queryHandle(twitter_handle);
-            System.out.println("Please enter a Twitter handle, do no include the '@' symbol (or 'done' to quit.)");
-            twitter_handle = scan.next();
+        System.out.print("Please enter a latitude (Decimal Degrees): ");
+        bigBird.setLatitude(Double.parseDouble(scan.next()));
+        if (((bigBird.getLatitude()) < -90) || ((bigBird.getLatitude()) > 90)) {
+            System.out.println("Sorry, check your latitude.  Valid latitude is between -90 and 90 degrees.");
+        } else {
+            System.out.println("Latitude set to " + bigBird.getLatitude() + "\n");
         }
-bigBird.saQuery("None");
+
+        System.out.print("Please enter a longitude (Decimal Degrees): ");
+        bigBird.setLongitude(Double.parseDouble(scan.next()));
+        if (((bigBird.getLongitude()) < -180) || ((bigBird.getLongitude()) > 180)) {
+            System.out.println("Sorry, check your latitude.  Valid longitude is between -180 and 180 degrees.");
+        } else {
+            System.out.println("Longitude set to " + bigBird.getLongitude() + "\n");
+        }
+        System.out.print("Please enter a search radius (in miles): ");
+        bigBird.setMiles(Integer.parseInt(scan.next()));
+        System.out.println("Search within " + bigBird.getMiles() + " miles of " +
+                bigBird.getLatitude() + " " + bigBird.getLongitude() + "\n");
+
+        System.out.print("Please enter a time-frame to search (YYYY-MM-DD): ");
+        bigBird.setTimeFrame(scan.next());
+
+        System.out.print("Please enter a term to search for: ");
+        bigBird.setSearchQuery(scan.next());
+        bigBird.saQuery(bigBird.getSearchQuery());
+
+
+//bigBird.saQuery("Spurs");
 
     }
 }
--- lib/src/Twitterer.java
+++ lib/src/Twitterer.java
@@ -10,79 +10,112 @@ import twitter4j.TwitterException;
 import java.io.IOException;
 import java.io.PrintStream;
 
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.SortedMap;
 
 public class Twitterer {
 
     private Twitter twitter;
     private PrintStream consolePrint;
     private List<Status> statuses;
+    private double latitude;
+    private double longitude;
+    private int miles;
+    private String timeFrame;
+    private String searchQuery;
 
 
-    public Twitterer(PrintStream console)
-
-    {
+    public Twitterer(PrintStream console) {
         twitter = TwitterFactory.getSingleton();
         consolePrint = console;
         statuses = new ArrayList<Status>();
     }
 
-    public void tweetOut(String message) throws TwitterException, IOException
-    {
+    public void tweetOut(String message) throws TwitterException, IOException {
         Status status = twitter.updateStatus(message);
         System.out.println("Successfully updated the status to [" + status.getText() + "].");
     }
 
     @SuppressWarnings("unchecked")
-    public void queryHandle(String handle) throws TwitterException, IOException
-    {
+    public void queryHandle(String handle) throws TwitterException, IOException {
         statuses.clear();
         fetchTweets(handle);
         int counter = statuses.size();
-        while (counter > 0);
-        {
+        while (counter > 0) {
             counter--;
-            System.out.println("Tweet #"+counter+": "+statuses.get(counter).getText());
+            System.out.println("Tweet #" + counter + ": " + statuses.get(counter).getText());
         }
     }
 
-public void fetchTweets(String handle) throws TwitterException, IOException
-{
-    Paging page = new Paging(1, 200);
-    int p = 1;
-    while (p <= 10)
-    {
-        page.setPage(p);
-        statuses.addAll(twitter.getUserTimeline(handle, page));
-        p++;
-    }
-}
-
-public void saQuery (String searchTerm)
-{
-    Query query = new Query(searchTerm);
-    query.setCount(100);
-    query.setGeoCode(new GeoLocation(0, 0), 500, Query.MILES);
-    query.setSince("2019-03-01");
-
-    try {
-        QueryResult result = twitter.search(query);
-        int counter = 0;
-        System.out.println("Count: " + result.getTweets().size());
-        for (Status tweet : result.getTweets()) {
-            counter++;
-            System.out.println("Tweet #" + counter + ": @" + tweet.getUser().getName() +
-                    "tweeted \"" + tweet.getText() + "\"");
+    public void fetchTweets(String handle) throws TwitterException, IOException {
+        Paging page = new Paging(1, 200);
+        int p = 1;
+        while (p <= 10) {
+            page.setPage(p);
+            statuses.addAll(twitter.getUserTimeline(handle, page));
+            p++;
         }
     }
-        catch(TwitterException e)
-        {
+
+    public void saQuery(String searchTerm) {
+        Query query = new Query(searchTerm);
+        query.setCount(100);
+        query.setGeoCode(new GeoLocation(getLatitude(), getLongitude()),getMiles(), Query.MILES);
+        query.setSince(getTimeFrame());
+
+        try {
+            QueryResult result = twitter.search(query);
+            int counter = 0;
+            System.out.println("Count: " + result.getTweets().size());
+            for (Status tweet : result.getTweets()) {
+                counter++;
+                System.out.println("Tweet #" + counter + ": @" + tweet.getUser().getName() +
+                        "tweeted \"" + tweet.getText() + "\"");
+            }
+        } catch (TwitterException e) {
             e.printStackTrace();
         }
         System.out.println();
+    }
+
+    public void setLatitude(double latIn) {
+        latitude = latIn;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLongitude(double longIn) {
+        longitude = longIn;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setMiles(int milesIn) {
+        miles = milesIn;
+    }
+
+    public int getMiles() {
+        return miles;
+    }
+
+    public void setTimeFrame(String timeIn) {
+        timeFrame = timeIn;
+    }
+
+    public String getTimeFrame() {
+        return timeFrame;
+    }
+
+    public void setSearchQuery(String searchIn) {
+        searchQuery = searchIn;
+    }
+
+    public String getSearchQuery() {
+        return searchQuery;
     }
 }
 
